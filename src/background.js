d2bd7ef0be4f10d8217077d9b4ab9ae49bcc2a63
Light Wu <wuli5@oocl.com>
2016-04-05 18:20:30 +0800

    bugfix about phase

--- src/background.js
+++ src/background.js
@@ -4,7 +4,8 @@
   chrome.extension.onConnect.addListener(function (port) {
     console.assert(port.name == "dict");
     port.onMessage.addListener(function (msg) {
-      $.get(url + msg.word, function (data) {
+      var content = decodeHtml(msg.word);
+      $.get(url + content, function (data) {
         var result = (pattern.exec(data))[1];
         port.postMessage({time: msg.time, word: result});
       });
